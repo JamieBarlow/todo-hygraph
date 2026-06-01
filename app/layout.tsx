11ed23a90351c5b4a7439a554993a359f307247c
JamieBarlow <jamiebarlow@gmail.com>
2026-06-01 20:54:02 +0100

    Add next-auth session provider. Update UI to reflect logged in status. Add sign out functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MainNav from "@/components/MainNav";
+import AuthSessionProvider from "@/components/SessionProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <MainNav />
-        {children}
+        <AuthSessionProvider>
+          <MainNav />
+          {children}
+        </AuthSessionProvider>
       </body>
     </html>
   );
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,20 +1,31 @@
-import { Button } from "./ui/button";
+"use client";
 
+import { useSession, signOut } from "next-auth/react";
+import { Button } from "./ui/button";
 import Link from "next/link";
 
 const MainNav = () => {
+  const { data: session } = useSession();
   return (
     <nav className="flex items-center w-full p-2">
       <Button size="lg">
         <Link href="/">Home Page</Link>
       </Button>
       <div className="ml-auto flex gap-2">
-        <Button size="lg">
-          <Link href="/login">Login</Link>
-        </Button>
-        <Button size="lg">
-          <Link href="/register">Register</Link>
-        </Button>
+        {session ? (
+          <Button size="lg" onClick={() => signOut({ callbackUrl: "/login" })}>
+            Logout
+          </Button>
+        ) : (
+          <>
+            <Button size="lg">
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button size="lg">
+              <Link href="/register">Register</Link>
+            </Button>
+          </>
+        )}
       </div>
     </nav>
   );
