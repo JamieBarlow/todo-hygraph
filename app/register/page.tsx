4d103a78aace52ca0118c3da8666842c9c7437ee
JamieBarlow <jamiebarlow@gmail.com>
2026-06-02 00:36:32 +0100

    Remove typo from register route

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,7 @@ export default function Register() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans dark:bg-black">
       <main className="flex flex-1 gap-6 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black">
-        <RegisterForm></RegisterForm>;
+        <RegisterForm></RegisterForm>
       </main>
     </div>
   );
